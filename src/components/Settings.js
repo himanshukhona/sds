226c6c2081224f82b75d7764ad917077300c019a
vaibhav kohli <vaibhav.khl@gmail.com>
2017-03-21 12:57:34 +0530

    ejected out of app, using recat router v2
    will upgrade or replace when we integrate redux

---
+++ src/components/Settings.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react'
+const Settings = () => <h1>Hello from Settings!</h1>
+
+export default Settings
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router'
+//import createBrowserHistory from 'history/createBrowserHistory'
 import App from './App';
+import Settings from './components/Settings'
+import NotFound from './components/NotFound'
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+//const history = createBrowserHistory()
+
+ReactDOM.render((
+  <Router history={hashHistory}>
+      <Route path="/" component={App}/>
+      <Route path="/settings" component={Settings}/>
+      <Route path='*' component={NotFound} />
+  </Router>
+), document.getElementById('root'));
